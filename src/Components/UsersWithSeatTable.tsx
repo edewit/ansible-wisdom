7183d9f20b84d5ce629d7dbc406800d2f833bdf3
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-07-24 15:39:08 +0200

    Revert "fixed lint"
    This reverts commit 516c68f71dd0fd27b00cae6987548e101f5212cc.

--- src/Components/UsersWithSeatTable.tsx
+++ src/Components/UsersWithSeatTable.tsx
@@ -59,6 +59,9 @@ export const UsersWithSeatTable = ({
   onPageChange,
   onRemoveSeat,
   onAddUser,
+  onSearchUsername,
+  onRemoveUsernameChip,
+  onRemoveUsernameChips,
   onClearAllFilters,
 }: UsersWithSeatTableProps) => {
   const breakpoint = 'lg';
