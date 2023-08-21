0146bbd9fcc97f8878c3d1d7e124e3242455cc3b
Adriana Cruz <acruzgon@redhat.com>
2023-08-21 07:43:25 -0600

    setting up cypress

---
+++ cypress.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "cypress";
+
+const fs = require("fs-extra");
+const path = require("path");
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve("cypress/config", `${file}.env.json`);
+  // check if file exists
+  if (!fs.existsSync(pathToConfigFile)) {
+    throw new Error(`Config file ${pathToConfigFile} does not exist`);
+  }
+
+  return fs.readJson(pathToConfigFile);
+}
+
+export default defineConfig({
+  retries: {
+    runMode: 3,
+    openMode: 0,
+  },
+  videoUploadOnPasses: false,  
+  viewportWidth: 1600,
+  viewportHeight: 1000,
+  e2e: {
+    setupNodeEvents(on, config) {
+      const file = config.env.configFile || "stage";
+      return getConfigurationByFile(file);      
+    },
+  },
+});
