281a2947ff19f9fe8e99ab6ec61ae9c6f8c8292a
ylahav <ylahav@redhat.com>
2023-09-12 12:25:47 +0300

    remove login file

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -36,7 +36,17 @@
 //   }
 // }
 
-import './login';
+function setCookiesForUILogin() {
+  Cypress.Cookies.debug(true);
+  cy.setCookie('cmapi_cookie_privacy', 'permit 1,2,3', { secure: true });
+  cy.setCookie('cmapi_gtm_bl', '', { secure: true });
+  cy.setCookie('notice_preferences', '2:', { secure: true });
+  cy.setCookie('notice_behavior', 'expressed,eu', { secure: true });
+  cy.setCookie('notice_gdpr_prefs', '0,1,2:', {
+    secure: true,
+    domain: 'redhat.com',
+  });
+}
 
 Cypress.Commands.add('login', (username) => {
   let password = ""
