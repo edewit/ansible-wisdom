656119effc7b0c69d75632cdb6d17620e0b6e3fb
ylahav <ylahav@redhat.com>
2023-09-20 14:27:26 +0300

    added ignoring uncaught exceptions

--- cypress/e2e/lightspeed-org-admin.cy.ts
+++ cypress/e2e/lightspeed-org-admin.cy.ts
@@ -4,7 +4,8 @@ describe('Navigate to the Prod Seats Administration page as lightspeed-org-admin
   });
 
   it('passes', () => {
-    cy.get('.pf-c-title', { timeout: 30000 }).should('include.text', 'Ansible Lightspeed with IBM watsonx Code Assistant');
+    cy.get('.pf-c-title').as('title');
+    cy.get('@title').should('include.text', 'Ansible Lightspeed with IBM watsonx Code Assistant');
     cy.get('.pf-l-level > div > p').should('include.text', 'This group contains all users assigned seats within your organization.');
   })
 })
--- cypress/e2e/lightspeed-test-user-2.cy.ts
+++ cypress/e2e/lightspeed-test-user-2.cy.ts
@@ -4,7 +4,8 @@ describe('Navigate to the Seats Administration page as lightspeed-test-user-2', 
   });
 
   it('passes', () => {
-    cy.get('.pf-c-title', { timeout: 30000 }).should('include.text', 'Ansible Lightspeed with IBM watsonx Code Assistant');
+    cy.get('.pf-c-title').as('title');
+    cy.get('@title').should('include.text', 'Ansible Lightspeed with IBM watsonx Code Assistant');
     cy.get('.pf-l-level > div > p').should('include.text', 'This group contains all users assigned seats within your organization.');
   })
 })
