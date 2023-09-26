ced9e16ba14c227fc16baa1c3efc581caf11a5ad
Adriana Cruz <acruzgon@redhat.com>
2023-09-26 10:21:14 -0600

    Test the search box in the users list

--- cypress/e2e/lightspeed-org-admin.cy.ts
+++ cypress/e2e/lightspeed-org-admin.cy.ts
@@ -1,12 +1,22 @@
 describe('Navigate to the Prod Seats Administration page as lightspeed-org-admin', () => {
-  before(() => {
+  beforeEach(() => {
     cy.login('lightspeed-org-admin');
   });
 
-  it('passes', () => {
+  it('happy path', () => {
     cy.get('.pf-c-title.pf-m-2xl').as('title');
     cy.get('@title').should('include.text', 'Ansible Lightspeed with IBM watsonx Code Assistant');
     cy.get('.pf-l-level > div > p').should('include.text', 'This group contains all users assigned seats within your organization.');
+  })
+
+  it('test the search box in the users list', () => {
+    cy.get('.pf-c-title', { timeout: 30000 }).as('title');
+    cy.get('[data-cy="users-table"]').as('users_table').should('be.visible')
+    cy.get('.pf-c-text-input-group__text-input').type('lightspeed-org-admin{enter}')
+    cy.get('@users_table').find('tbody [data-label="Username"]').each(($e1, index, $list) =>{
+      expect($list).length(1)
+      expect($e1.text()).to.equal('lightspeed-org-admin')
+    })
   })
 })
 
