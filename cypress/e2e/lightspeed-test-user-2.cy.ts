36e7b42d17551285e772a5626fc6f2431f881c03
Adriana Cruz <acruzgon@redhat.com>
2023-10-17 12:37:32 -0600

    Test pagination in seat admin page with a non-admin user

--- cypress/e2e/lightspeed-test-user-2.cy.ts
+++ cypress/e2e/lightspeed-test-user-2.cy.ts
@@ -4,37 +4,28 @@ describe('Navigate to the Seats Administration page as lightspeed-test-user-2', 
   });
 
   it('passes', () => {
-    cy.get('.pf-c-title').as('title');
+    cy.get('.pf-c-title', { timeout: 30000 }).as('title');
     cy.get('@title').should('include.text', 'Ansible Lightspeed with IBM watsonx Code Assistant');
     cy.get('.pf-l-level > div > p').should('include.text', 'This group contains all users assigned seats within your organization.');
   })
 
   it('sorting', () => {
-    const dataList = new Array;
-    const allowed = 30
-    for (var i = 0; i < allowed; i++) {
-      dataList[i] = {
-        "account_username": "lightspeed-test-user-" + i,
-        "first_name": "Test" + i,
-        "last_name": "Test" + i,
-        "status": "Active",
-      }
-    }
-
-    cy.intercept('GET', '/api/entitlements/v1/seats?status=Active*', {
-      allowed: allowed,
-      consumed: 30,
-      meta: { count: 30 },
-      data: dataList
-    }).as('mockUsers');
-    cy.wait('@mockUsers')
-
-    cy.get('.pf-c-title').as('title');
+    cy.mockUsers(30)
+    cy.get('.pf-c-title', { timeout: 30000 }).as('title');
     cy.get('@title').should('include.text', 'Ansible Lightspeed with IBM watsonx Code Assistant');
     cy.get(':nth-child(2) > .pf-c-table__button').click()
     cy.sorting('[data-cy="users-table"] tbody [data-label="Username"]', 'asc');
     cy.get(':nth-child(2) > .pf-c-table__button').click()
     cy.sorting('[data-cy="users-table"] tbody [data-label="Username"]', 'des');
+
+
+  })
+
+  it('pagination', () => {
+    cy.mockUsers(50)
+    cy.get('.pf-c-title', { timeout: 30000 }).as('title');
+    cy.get('@title').should('include.text', 'Ansible Lightspeed with IBM watsonx Code Assistant');
+    cy.pagination();
 
 
   })
